5fb2040b98bd9ad60d954c7ffa75693f18c8885a
Max Glenister <moglenstar@gmail.com>
2013-09-30 14:17:40 +0100

    Removed jquery dependency

--- popup.js
+++ popup.js
@@ -1,12 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    $('#note').val(localStorage.getItem('note'));
-    $('#note').on('keydown keyup focus blur', function () {
-        localStorage.setItem('note',$('#note').val());
-    });
-    $('#clear').on('click', function (e) {
-        e.preventDefault();
+    var textarea = document.getElementById('note');
+    var button = document.getElementById('clear');
 
+    var store_value = function() {
+        localStorage.setItem('note', textarea.value);
+    };
+    var read_value = function() {
+        localStorage.getItem('note');
+    };
+    var clear_value = function() {
         localStorage.removeItem('note');
-        $('#note').val('');
-    });
+        textarea.value = '';
+    };
+
+    textarea.onkeydown = function(evt) {
+        evt = evt || window.event;
+        store_value();
+    };
+    textarea.onkeyup = function(evt) {
+        evt = evt || window.event;
+        store_value();
+    };
+
+    textarea.value = localStorage.getItem('note');
+
+    button.onclick = clear_value;
 });
